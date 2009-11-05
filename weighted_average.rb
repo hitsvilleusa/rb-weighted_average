143a335c606e5b5c896ea58aead79f23b7f3bc79
Chris Morris <hitsvilleusa@yahoo.com>
2009-11-04 17:32:07 -0800

    adding the weighted average file

---
+++ weighted_average.rb
@@ -0,0 +1,19 @@
+module Enumerable
+  def weighted_average(average_attribute, weight_attribute)
+    weight = 0.0
+    sum    = 0.0
+    
+    each do |item|
+      average_value = item.send(average_attribute)
+      weight_value  = item.send(weight_attribute)
+      
+      next if average_value.nil? || average_value == 0
+      next if weight_value.nil?  || weight_value  == 0
+      
+      sum    += weight_value * average_value 
+      weight += weight_value
+    end
+
+    weight == 0.0 ? 0 : sum / weight
+  end
+end
